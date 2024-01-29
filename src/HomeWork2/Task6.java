6f369dc482c8117d8f03fd9e5019c3685b96573e
monocvet <monocvet@mail.ru>
2024-01-29 13:51:11 +0300

    2: Tasks 1-8 completed

---
+++ src/HomeWork2/Task6.java
@@ -0,0 +1,13 @@
+package HomeWork2;
+
+public class Task6 {
+    /**
+     * 6) Выведите в консоль все четные числа от 0 до 17 с помощью цикла.
+     * (использовать в теле цикла вложенное условие if с проверкой, что остаток от деления на 2 равен нулю)
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) System.out.println(i);
+        }
+    }
+}
---
+++ src/HomeWork2/Task7.java
@@ -0,0 +1,12 @@
+package HomeWork2;
+
+public class Task7 {
+    /**
+     * 7) С помощью цикла for выведите в консоль все числа от 10 до 1.
+     */
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
